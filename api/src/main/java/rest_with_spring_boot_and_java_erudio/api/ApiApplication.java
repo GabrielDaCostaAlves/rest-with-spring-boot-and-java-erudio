6c0d81546d6dc422c1f453c9e30cfa300cdfffd1
Gabriel Da Costa Alves <109757880+GabrielDaCostaAlves@users.noreply.github.com>
2024-10-23 17:06:58 -0300

    initial commit

---
+++ api/src/main/java/rest_with_spring_boot_and_java_erudio/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package rest_with_spring_boot_and_java_erudio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/rest_with_spring_boot_and_java_erudio/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rest_with_spring_boot_and_java_erudio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
